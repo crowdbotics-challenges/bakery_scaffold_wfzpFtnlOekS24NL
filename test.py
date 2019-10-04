21b192fa62ed78e6d33e5fd76f063d6bef49492f
crowdbotics-challenges <andre@crowdbotics.com>
2019-10-04 13:43:55 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -202,7 +202,7 @@ class AssessmentTestCases(unittest.TestCase):
         return res.group().split("'")[1]
 
     def _get_url(self):
-        return "https://pacific-journey-62283.herokuapp.com/"
+        return "https://immense-dawn-27112.herokuapp.com/"
 
     def _check_webhook_data(self):
         x = urllib.request.urlopen(f'{self._get_url()}/payment_intent')
